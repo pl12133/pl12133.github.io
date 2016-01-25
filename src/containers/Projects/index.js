605684168f514db8badfe25843586a5775f7d3be
pl12133 <krirken@gmail.com>
2016-01-25 04:15:00 -0500

    Setup containers

---
+++ src/containers/Projects/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars*/
+import React, { Component } from 'react';
+/* eslint-enable no-unused-vars*/
+
+const Projects = () => (
+  <div style={ {
+    width: '100%',
+    height: '100vh',
+    border: '2px solid green' } }>
+      {'Projects Section!'}
+  </div>
+);
+
+export default Projects;
---
+++ src/containers/Skills/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars*/
+import React, { Component } from 'react';
+/* eslint-enable no-unused-vars*/
+
+const Skills = () => (
+  <div style={ {
+    width: '100%',
+    height: '100vh',
+    border: '2px solid red' } }>
+      {'Skills Section!'}
+  </div>
+);
+
+export default Skills;
