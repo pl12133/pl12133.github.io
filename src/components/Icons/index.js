0738916bc07ddbe421a1bfaa64a6100b9ace66db
pl12133 <krirken@gmail.com>
2016-01-25 04:04:12 -0500

    SVG Icons implemented from [DevIcons](http://devicon.fr/)

---
+++ src/components/Icons/index.js
@@ -0,0 +1,34 @@
+import VimIcon from './VimIcon/';
+import SassIcon from './SassIcon/';
+import ReactIcon from './ReactIcon/';
+import PHPIcon from './PHPIcon/';
+import NodeIcon from './NodeIcon/';
+import LinuxIcon from './LinuxIcon/';
+import JQueryIcon from './JQueryIcon/';
+import JavaScriptIcon from './JavaScriptIcon/';
+import HTML5Icon from './HTML5Icon/';
+import GitHubIcon from './GitHubIcon/';
+import CSS3Icon from './CSS3Icon/';
+import CPlusPlusIcon from './CPlusPlusIcon/';
+import PythonIcon from './PythonIcon/';
+import BootstrapIcon from './BootstrapIcon/';
+
+// The order of the buttons here is how they will appear in the <IconBar>
+
+export default {
+  HTML5Icon,
+  CSS3Icon,
+  JavaScriptIcon,
+  NodeIcon,
+  CPlusPlusIcon,
+  PythonIcon,
+  PHPIcon,
+  ReactIcon,
+  JQueryIcon,
+  SassIcon,
+  BootstrapIcon,
+  GitHubIcon,
+  LinuxIcon,
+  VimIcon
+};
+
