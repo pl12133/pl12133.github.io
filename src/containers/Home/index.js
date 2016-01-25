258a01c4d24ea89a0e1b93c366343abe387138e4
pl12133 <krirken@gmail.com>
2016-01-25 04:13:21 -0500

    Three `<FullscreenPanel>`s in a row

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,14 +1,43 @@
 /* eslint-disable no-unused-vars*/
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 /* eslint-enable no-unused-vars*/
 
-import { Jumbotron } from 'react-bootstrap';
+import About from 'containers/About/';
+import Skills from 'containers/Skills/';
+import Projects from 'containers/Projects/';
+import FullscreenPanel from 'components/FullscreenPanel/';
+
+const imageURLS = [
+  'http://assets.sheratonlincolnharbor.com/lps/assets/u/1600x900iStock_000041057230XXXLarge.jpg',
+  'http://loving-newyork.com/wp-content/uploads/2015/10/empire-state-building-new-york.jpg',
+  'http://justgoodvibe.com/wp-content/uploads/2015/12/one-world-trade-center-6.jpg'
+];
 
 const Home = () => (
   <div style={ { width: '100%' } }>
-    <Jumbotron style={ { maxWidth: '20%', textAlign: 'center', margin: '0 auto' } }>
-      {'React With The Hotness!'}
-    </Jumbotron>
+    <FullscreenPanel
+      color='blue'
+      downArrow
+      imageURL={imageURLS[0]}
+    >
+      <About />
+    </FullscreenPanel>
+
+    <FullscreenPanel
+      color='orange'
+      downArrow
+      imageURL={imageURLS[1]}
+    >
+      <Skills />
+    </FullscreenPanel>
+
+    <FullscreenPanel
+      color='purple'
+      downArrow
+      imageURL={imageURLS[2]}
+    >
+      <Projects />
+    </FullscreenPanel>
   </div>
 );
 
