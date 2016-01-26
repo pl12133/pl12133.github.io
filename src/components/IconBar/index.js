849148bac1598f1ba1fcc898181650e927b0b4cc
pl12133 <krirken@gmail.com>
2016-01-25 22:46:05 -0500

    Split icons into responsive grid

--- src/components/IconBar/index.js
+++ src/components/IconBar/index.js
@@ -3,14 +3,28 @@ import React, { Component, PropTypes } from 'react';
 /* eslint-enable no-unused-vars*/
 
 import Icons from 'components/Icons/';
+import { Grid, Row, Col } from 'react-bootstrap';
 
 const IconBar = () => {
-  let children = Object.keys(Icons).map(key => {
+  let children = Object.keys(Icons).map((key, index) => {
     let Icon = Icons[key];
     return (
-      <Icon key={key} style={ { display: 'inline-block', width: '5vw' } } />
+      <Icon key={key} style={ { display: 'inline-block', width: '10%' } } />
     );
   });
+  let rowsOfChildren = (
+    <Grid>
+      <Row>
+        <Col xs={12} sm={6}>
+          {children.slice(0, 7)}
+        </Col>
+        <Col xs={12} sm={6}>
+          {children.slice(7)}
+        </Col>
+      </Row>
+    </Grid>
+  );
+
   return (
     <div style={ {
       position: 'absolute',
@@ -19,7 +33,7 @@ const IconBar = () => {
       background: 'linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6))',
       textAlign: 'center' } }
     >
-      {children}
+      {rowsOfChildren}
     </div>
   );
 };
