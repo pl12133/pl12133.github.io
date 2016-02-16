1dd9796b84f728a4b816d91ae400ec716b8bb7aa
pl12133 <krirken@gmail.com>
2016-02-15 21:33:11 -0500

    Move components which are representative of a Route into `routes` folder

---
+++ src/routes/App/index.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars*/
+import React, { Component } from 'react';
+/* eslint-enable no-unused-vars*/
+
+// class App extends Component {
+//   constructor (props) {
+//     super(props);
+//   }
+//   render () {
+//     let { children } = this.props;
+//     return (
+//       <div>
+//         {children}
+//       </div>
+//     );
+//   }
+// }
+
+const App = ({children}) => (
+  <div>
+    {children}
+  </div>
+);
+
+export default App;
