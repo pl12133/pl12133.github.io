755910305d4b63a7fb1a06fe7d64dea9d4ac4c8e
pl12133 <krirken@gmail.com>
2016-02-23 21:26:17 -0500

    Add twitch-clipper-demo images, use a `<Grid>` for layout on `<Projects>`

--- src/containers/Projects/index.js
+++ src/containers/Projects/index.js
@@ -2,13 +2,22 @@
 import React, { Component } from 'react';
 /* eslint-enable no-unused-vars*/
 
-import { Jumbotron } from 'react-bootstrap';
+import { Grid, Row, Col, Jumbotron } from 'react-bootstrap';
 import ScrimOverlay from 'components/ScrimOverlay/';
 
+// Width is adjusted by -30px for bootstrap grid mergins
 const DemoVideo = ({overlayText, href, videoSrc, fallbackImageSrc}) => (
-  <a href={href}>
-    <ScrimOverlay text={overlayText} />
-    <video style={ { width: '100%' } } autoPlay loop poster={fallbackImageSrc}>
+  <a href={href} style={ { display: 'block' } }>
+    <ScrimOverlay
+      text={overlayText}
+      style={ { width: 'calc(100% - 30px)' } }
+    />
+    <video
+      style={ { width: '100%' } }
+      autoPlay
+      loop
+      poster={fallbackImageSrc}
+    >
       <source src={videoSrc} type='video/webm' />
     </video>
   </a>
@@ -34,35 +43,41 @@ const containerStyles = {
 };
 
 const Projects = () => (
-  <div>
-    <JumboHeader>
-      {'A few things I\'ve been working on'}
-    </JumboHeader>
-    <div style={ {
-      ...containerStyles,
-      marginLeft: '10%'
-    } }
-    >
-      <DemoVideo
-        overlayText={'React Solitaire'}
-        href={"https://pl12133.github.io/react-solitaire"}
-        videoSrc={'dist/solitaire-demo.webm'}
-        fallbackImageSrc={'dist/solitaire-demo.png'}
-      />
-    </div>
-    <div style={ {
-      ...containerStyles,
-      marginLeft: '20%'
-    } }
-    >
-      <DemoVideo
-        overlayText={'A personal watch collection'}
-        href={'https://pl12133.github.io/watch-collection'}
-        videoSrc={'dist/watch-collection-demo.webm'}
-        fallbackImageSrc={'dist/watch-collection-demo.png'}
-      />
-    </div>
-  </div>
+  <Grid>
+    <Row>
+      <Col xs={12}>
+        <JumboHeader>
+          {'A few things I\'ve been working on'}
+        </JumboHeader>
+      </Col>
+    </Row>
+    <Row style={ { marginTop: '1%' } }>
+      <Col xs={6} md={4}>
+        <DemoVideo
+          overlayText={'React Solitaire'}
+          href={"https://pl12133.github.io/react-solitaire"}
+          videoSrc={'dist/solitaire-demo.webm'}
+          fallbackImageSrc={'dist/solitaire-demo.png'}
+        />
+      </Col>
+      <Col xs={6} md={4}>
+        <DemoVideo
+          overlayText={'watch-collection'}
+          href={'https://pl12133.github.io/watch-collection'}
+          videoSrc={'dist/watch-collection-demo.webm'}
+          fallbackImageSrc={'dist/watch-collection-demo.png'}
+        />
+      </Col>
+      <Col xs={6} md={4}>
+        <DemoVideo
+          overlayText={'node-twitch-clipper'}
+          href={'https://pl12133.github.io/node-twitch-clipper-demo'}
+          videoSrc={''}
+          fallbackImageSrc={'dist/twitch-clipper-demo.png'}
+        />
+      </Col>
+    </Row>
+  </Grid>
 );
 
 export default Projects;
